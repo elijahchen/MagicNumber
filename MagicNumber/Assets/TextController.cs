cabfb61dadea63106fe774aaf0d58f6ab3cbdd69
elijahchen <code.elijahchen@gmail.com>
2016-11-29 20:57:40 -0500

    - Experimenting with 2D UI

---
+++ MagicNumber/Assets/TextController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        text.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            text.text = "I will now guess the number you have in mind... with no more than 10 guesses!";
+        }
+	}
+}
